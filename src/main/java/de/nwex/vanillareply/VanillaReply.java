2bffea9333ff366c9be25d273d756e215a896d7f
networkException <git@nwex.de>
2020-08-25 17:02:14 +0200

    Meta: Initialized repository from fabric-example-mod

---
+++ src/main/java/de/nwex/vanillareply/VanillaReply.java
@@ -0,0 +1,11 @@
+package de.nwex.vanillareply;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class VanillaReply implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
